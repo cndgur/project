b3fd5ba004a39a65a95ab9355ba5c0b04512d20e
song-h-g0907 <skp0213@gmail.com>
2024-10-10 18:14:39 +0900

    .

---
+++ lc_project/src/main/java/com/lc/project/map/controller/TagRecommendController.java
@@ -0,0 +1,61 @@
+package com.lc.project.map.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.lc.project.map.service.MapService;
+import com.lc.project.map.service.MapServiceImpl;
+import com.lc.project.travel.model.vo.Travel;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TagRecommendController
+ */
+public class TagRecommendController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TagRecommendController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MapService mService = new MapServiceImpl();
+		
+		String personnel = request.getParameter("personnel");
+		String location = request.getParameter("location");
+		String theme = request.getParameter("theme");
+		
+		System.out.println(request.getParameter("personnel"));
+		System.out.println(request.getParameter("location"));
+		System.out.println(request.getParameter("theme"));
+		
+		request.setAttribute("personnel", personnel);
+	    request.setAttribute("location", location);
+	    request.setAttribute("theme", theme);
+	    
+	    ArrayList<Travel> trList = mService.selectTravelTag(personnel, location, theme);
+	    request.setAttribute("tr", trList);
+	    
+	    request.getRequestDispatcher("views/map/tagRecommendPage.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ lc_project/src/main/java/com/lc/project/travel/controller/travelInfoController.java
@@ -0,0 +1,61 @@
+package com.lc.project.travel.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.lc.project.member.model.vo.Member;
+import com.lc.project.travel.model.vo.Travel;
+import com.lc.project.travel.model.vo.tReview;
+import com.lc.project.travel.service.TravelService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class travelInfoController
+ */
+public class travelInfoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public travelInfoController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String trName = request.getParameter("travel");
+		TravelService tServ = new TravelService();
+		Travel t = tServ.selectTravel(trName);
+		String[] strArr = t.getTrAddress().split(" ");
+		String tAdd = strArr[0]+" "+strArr[1];
+		ArrayList<Travel> tlist = tServ.selectNearbyTravel(tAdd,trName);
+		for(Travel tra : tlist) {
+			tra.setTrAddress(tAdd);
+		}
+		ArrayList<tReview> rlist = tServ.selectTReview(trName);
+		HttpSession session = request.getSession();
+		request.setAttribute("t", t);
+		request.setAttribute("tlist", tlist);
+		request.setAttribute("rlist", rlist);
+		
+		request.getRequestDispatcher("views/detail/tr_infoPage.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
